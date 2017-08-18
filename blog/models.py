244013cc1db3255a1a34488b5d9cc2b989fbce26
Logan-Ruf <cloganruf@gmail.com>
2017-08-18 00:03:03 -0700

    Blog Clone Project Part Two

---
+++ blog/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+from django.utils import timezone
+from django.core.urlresolvers import reverse
+# Create your models here.
+
+class Post(models.Model):
+    author = models.ForeignKey('auth.User')
+    title = models.CharField(max_length = 200)
+    text = models.TextField()
+    create_date = models.DateTimeField(default=timezone.now())
+    published_date = models.DateTimeField(blank=Ture,null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def approve_comments(self):
+        return self.comments.filter(approve_comment=True)
+
+    def get_absolute_url(self):
+        return reverse("post_detail",kwargs={'pk':self.pk})
+
+    def __str__(self):
+        return self.title
+
+class Comment(model.Model):
+    post = models.ForeignKey('blog.Post',related_name'comments')
+    author = models.CharField(max_length=200)
+    text = models.TextField()
+    create_date = models.DateTimeField(default=timezone.now())
+    approved_comment = models.BooleanField(default=False)
